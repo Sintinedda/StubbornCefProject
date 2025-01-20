2b20d852ee271e322ee859213872a7fff25d5460
Jeremy Aboudan <aboudan.jeremy@gmail.com>
2025-01-20 10:12:19 +0100

    add link to top products from home page; add price filter to products list

--- src/Repository/SweatShirtRepository.php
+++ src/Repository/SweatShirtRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Data\SearchData;
 use App\Entity\SweatShirt;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -16,19 +17,40 @@ class SweatShirtRepository extends ServiceEntityRepository
         parent::__construct($registry, SweatShirt::class);
     }
 
-        /**
-         * @return SweatShirt[] Returns an array of SweatShirt objects
-         */
-        public function findByExampleField($value): array
-        {
-            return $this->createQueryBuilder('s')
-                ->andWhere('s.exampleField = :val')
-                ->setParameter('val', $value)
-                ->orderBy('s.id', 'ASC')
-                ->setMaxResults(10)
-                ->getQuery()
-                ->getResult()
-           ;
+    /**
+     * @return SweatShirt[] Returns an array of SweatShirt objects
+     */
+    public function findByExampleField($value): array
+    {
+        return $this->createQueryBuilder('s')
+            ->andWhere('s.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('s.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+      }
+
+      public function findSearch(SearchData $search): array
+      {
+        $query = $this
+            ->createQueryBuilder('sweat')
+            ->select('sweat');
+
+        if (!empty($search->min)) {
+            $query = $query
+                ->andWhere('sweat.price >= :min')
+                ->setParameter('min', $search->min);
+        }
+
+        if (!empty($search->max)) {
+            $query = $query
+                ->andWhere('sweat.price <= :max')
+                ->setParameter('max', $search->max);
+        }
+
+        return $query->getQuery()->getResult();
       }
 
     //    public function findOneBySomeField($value): ?SweatShirt
